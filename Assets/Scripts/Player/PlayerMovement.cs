a7f57fed16436687f1e50c5744606cf352e70ddf
firehappy <cfy1123580901@163.com>
2025-05-28 08:57:14 +0800

    PlayerMove

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SimplePlayerController : MonoBehaviour
+{
+    public float runSpeed = 5f;
+
+    private Vector2 inputDirection;
+    private Rigidbody2D rb;
+    private PlayerInput inputControl;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        inputControl = new PlayerInput(); // 自动生成的输入类
+    }
+
+    private void OnEnable()
+    {
+        inputControl.Enable();
+    }
+
+    private void OnDisable()
+    {
+        inputControl.Disable();
+    }
+
+    private void Update()
+    {
+        inputDirection = inputControl.Player.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(inputDirection.x * runSpeed, inputDirection.y * runSpeed); // 四向
+    }
+}
